94be4b51ea2a0a31dbbdde4095656bca4279fdb8
斯杰 <devhacker520@hotmail.com>
2013-11-10 15:51:00 +0800

    项目重建

---
+++ config/config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+module.exports = {
+    //服务器设置
+    listenPort:3000,//监听端口
+    baseDir: path.join(__dirname, '/..'),//服务目录地址
+    clusterNum:3, //开启子的进程数
+    staticTime:1000*60*60*24,//静态文件客户端缓存时间（默认一天）
+    //session
+    session:true,//是否开启服务器session
+    sessionId:'harborsId',//session存储的cookie名字
+    SessionSurvavil:1000*60*60,//session信息默认客户端误操作后保留的有效时间（默认1小时）
+    clearSession:1000*60*30,//session清理时间（默认值1小时）
+    //主机设置
+    vhost:false,//是否开启虚拟主机服务，开启的话默认会寻找controller目录下的虚拟主机目录
+    serverType:'static',//主机类型，动态:'dynamic'，静态响应式服务器:'static'.只有在关闭vhost的时候生效
+    //开发设置
+    deBug:true//调试模式，自动打印出运行状态流程等信息
+};
---
+++ controller/filter.js
@@ -0,0 +1,11 @@
+module.exports = function(req,res){
+//    res.setSession({type:'123',language:'china'});
+//    console.log(req.Session);
+//    res.delSession({type:null});
+//    for(var i=0;i<100000000;i++){
+//        var r = Math.random();
+//    }
+    res.display('administrator/test.html');
+//    res.writeHead(200, {"Content-Type": "text/plain", "Connection": "close"});
+//    res.end("1234");
+};
