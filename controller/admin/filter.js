f0878dc6294a176e0e6b5a5ed96e6bfb78b3d090
斯杰 <devhacker520@hotmail.com>
2013-11-19 22:37:48 +0800

    更改架构，进度20%

---
+++ controller/admin/filter.js
@@ -0,0 +1,21 @@
+module.exports = function(req,res){
+
+    harbors.route = require(harbors.config.baseDir+'/config/route');
+
+    //增加session信息
+    //res.setSession({type:'123',language:'china'});
+
+    //打印session信息
+    //console.log(req.Session);
+
+    //删除session信息
+    //res.delSession({type:null});
+
+    //中转路由（传递给其他控制器，记得做容错）
+    //require('./'+req.route[0])(req,res);
+
+    res.delSession(["abc"]);
+    console.log(req.Session)
+    //显示动态模板
+    res.render('/admin/login.jade',{route:harbors.route});
+};
