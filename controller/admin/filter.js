ed14eacc11c8ff3b933e2c6210cc3b42e09af4cf
wangsijie <devhacker520@hotmail.com>
2013-11-26 10:52:11 +0800

    重構初始化流程後的功能恢復，封裝過程改爲異步

--- controller/admin/filter.js
+++ controller/admin/filter.js
@@ -1,21 +1,21 @@
 module.exports = function(req,res){
 
-    harbors.route = require(harbors.config.baseDir+'/config/route');
+//    harbors.route = require(harbors.config.baseDir+'/config/route');
 
     //增加session信息
     //res.setSession({type:'123',language:'china'});
 
     //打印session信息
-    //console.log(req.session);
+    console.log(req.session);
 
 
     //中转路由（传递给其他控制器，记得做容错）
     //require('./'+req.route[0])(req,res);
 
-    harbors.mysql('select * from g',function(err,data){
-        console.log(data);
-    });
-    console.log(req.session);
+//    harbors.mysql('select * from g',function(err,data){
+//        console.log(data);
+//    });
+//    console.log(req.session);
     res.setSession({abc:123},10);
 //    res.mysql('use itharbors');
 //    res.mysql('select * from users',function(err,res){
