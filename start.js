f5200841803cfed3c68cb821994cdf3c0dba7142
Wang Sijie <devhacker520@hotmail.com>
2014-01-06 14:54:19 +0800

    测试、修改重写样例

--- start.js
+++ start.js
@@ -7,15 +7,11 @@ harbors.config('./config/vhost.ini');
 //harbors.module('mysql',require('./module/mysql'));//自定义外部模块
 
 //harbors.rewrite('index',function(req){//自定义一个名为index的重写模块（可在配置中引用）
-//    var _sp = req.url.split('?');
-//    var _get = querystring.parse(_sp[1]);
-//    return {
-//        Module:'index_.js',
-//        Extname:'',
-//        Url:'/',
-//        File:'',
-//        Get:_get
-//    };
+//harbors.rewrite('develop',function(url){
+//    if(!/(static)\//.test(url))
+//        return '/index';
+//    else
+//        return url;
 //});
 
 harbors.start();
