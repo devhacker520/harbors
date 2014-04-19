8e849b8455bf0325f5b9ea3a9885907be4628868
VisualSJ <itharbors@outlook.com>
2014-04-19 22:27:48 +0800

    Add cluster modules

--- develop.js
+++ develop.js
@@ -1,16 +1,49 @@
 var harbors = require('harbors');
 
-var handle = harbors.Handle.create(function(req, res){
-    res.end('host is not found!');
+var _p = harbors.Cluster.create();
+
+_p.setTask('createServer', function(){
+    var handle = harbors.Handle.create(function(req, res){
+        res.end('host is not found!');
+    });
+
+    handle.addDomain('*.test.com', function(req, res){
+        res.end('a.test.com');
+    });
+
+    handle.addDomain('localhost', function(req, res){
+        res.end('localhost');
+    });
+
+    handle.addDomain('127.0.0.1', function(req, res){
+        res.end('127.0.0.1');
+    });
+
+    var _ = harbors.Server.create('http', '127.0.0.1', 9000, function(){});
+    _.handle(handle);
 });
 
-handle.addDomain('a.test.com', function(req, res){
-    res.end('localhost');
+_p.fork('createServer');
+
+_p.setTask('createServer2', function(){
+    var handle = harbors.Handle.create(function(req, res){
+        res.end('host is not found!');
+    });
+
+    handle.addDomain('*.test.com', function(req, res){
+        res.end('a.test.com');
+    });
+
+    handle.addDomain('localhost', function(req, res){
+        res.end('localhost');
+    });
+
+    handle.addDomain('127.0.0.1', function(req, res){
+        res.end('127.0.0.1');
+    });
+
+    var _ = harbors.Server.create('http', '127.0.0.1', 9001, function(){});
+    _.handle(handle);
 });
 
-handle.addDomain('127.0.0.1', function(req, res){
-    res.end('127.0.0.1');
-});
-
-var _ = harbors.Server.create('http', '127.0.0.1', 9000, function(){});
-_.handle(handle);
+_p.fork('createServer2');
